08365b0546692145a458981e988428c896c4a685
danzeph <danzeph00@gmail.com>
2025-11-25 23:49:14 +0000

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,1 +1,18 @@
+-- Finding the total number of bookings by each user with COUNT and GROUP BY clause
+-- Used Join to show users with zero booings
+
+SELECT u.user_id, 
+  u.name, 
+  COUNT(b.booking_id) AS "Number of Bookings"
+FROM users u
+LEFT JOIN booking b 
+ON u.user_id = b.user_id
+GROUP BY user_id, name;
+
+-- Ranking properties based on the total number of bookings 
+-- Using window function RANK 
+-- Using Join also to show properties with zero bookings
+
+
+
 
