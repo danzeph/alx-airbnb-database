9e74b33c5fa1d01562841b57b128196a2328f755
danzeph <danzeph00@gmail.com>
2025-11-26 00:45:20 +0000

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -13,6 +13,21 @@ GROUP BY user_id, name;
 -- Using window function RANK 
 -- Using Join also to show properties with zero bookings
 
+SELECT 
+p.property_id, 
+p.name AS property_name,
+COUNT(b.booking_id) AS total_bookings,
+RANK() OVER (
+ORDER BY COUNT(b.booking_id) DESC
+)  AS rank1,
+ROW_NUMBER() OVER (
+ORDER BY COUNT(b.booking_id) DESC
+)  AS row_rank
+FROM property p
+LEFT JOIN booking  b
+ON  p.property_id = b.property_id
+GROUP BY p.property_id, p.name;
+
 
 
 
