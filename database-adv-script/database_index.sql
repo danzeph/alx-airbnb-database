43decda2d3fd7d318a4a0735993c703be0dce246
danzeph <danzeph00@gmail.com>
2025-11-27 22:42:35 +0000

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,12 +1,18 @@
 --Indexes on the user table
 --user_id as our primary is automatically indexed
-CREATE INDEX urole ON users(role);
-CREATE INDEX ON users(created_at);
+CREATE INDEX indx_users_email ON users(role);
+CREATE INDEX inx_users_role ON users(created_at);
 
+
+--Indexes on the property table
+--property_id primary is automatically indexed
+CREATE INDEX idx_property_location ON property(location);
+CREATE INDEX idx_property_pricepernight ON property(pricepernight);
 
 --Indexes on the user table
---user_id as our primary is automatically indexed
-
-
---Indexes on the user table
---user_id as our primary is automatically indexed
+--booking_id primary is automatically indexed
+CREATE INDEX idx_booking_property_id ON booking(property_id);
+CREATE INDEX idx_booking_user_id ON booking(user_id);
+CREATE INDEX idx_booking_start_date ON booking(start_date);
+CREATE INDEX idx_booking_end_date ON booking(end_date);
+CREATE INDEX idx_booking_status ON booking(status);
