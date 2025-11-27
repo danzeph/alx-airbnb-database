c24502a76f38c612c956affe4854ff4586b65960
danzeph <danzeph00@gmail.com>
2025-11-27 21:08:50 +0000

    Create database_index.sql

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,12 @@
+--Indexes on the user table
+--user_id as our primary is automatically indexed
+CREATE INDEX urole ON users(role);
+CREATE INDEX ON users(created_at);
+
+
+--Indexes on the user table
+--user_id as our primary is automatically indexed
+
+
+--Indexes on the user table
+--user_id as our primary is automatically indexed
