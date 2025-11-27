3ae0a326f893dd1da2fe06b350b82772de03c6f3
danzeph <danzeph00@gmail.com>
2025-11-27 21:48:22 +0000

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,21 +1,21 @@
 --Query to find all properties where the average rating is greater than 4.0 using a sub query
 SELECT *
-FROM properties
+FROM property
 WHERE property_id IN  (
   SELECT property_id 
-  FROM ratings
+  FROM review
   GROUP BY property_id 
   HAVING AVG(rating ) > 4.0
-  )
+  );
 
 --Correlated subqery to find all users who have made more than 3 bookings
 
 SELECT * 
-FROM USERS u
+FROM users u
 WHERE (
   SELECT COUNT(booking_id)
-  FROM bookings b
+  FROM booking b
   WHERE b.user_id =u.user_id 
   )
-  > 3.0
+  > 3.0;
   
