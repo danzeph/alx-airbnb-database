f617b8c0837ee0bc6b34e50912da310eb1c86f02
danzeph <danzeph00@gmail.com>
2025-11-27 21:55:36 +0000

    Fix SQL query to count bookings and group by first name

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -2,12 +2,12 @@
 -- Used Join to show users with zero booings
 
 SELECT u.user_id, 
-  u.name, 
-  COUNT(b.booking_id) AS "Number of Bookings"
+  u.first_name,host 
+  COUNT(b.booking_id) AS "booking counts"
 FROM users u
 LEFT JOIN booking b 
 ON u.user_id = b.user_id
-GROUP BY user_id, name;
+GROUP BY user_id, first_name;
 
 -- Ranking properties based on the total number of bookings 
 -- Using window function RANK 
