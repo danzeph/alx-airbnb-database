dbf664b6fdc22009b28c619351285a280b92ada9
danzeph <danzeph00@gmail.com>
2025-11-27 22:01:52 +0000

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -2,31 +2,37 @@
 -- Used Join to show users with zero booings
 
 SELECT u.user_id, 
-  u.first_name,host 
+  u.first_name,role 
   COUNT(b.booking_id) AS "booking counts"
 FROM users u
 LEFT JOIN booking b 
 ON u.user_id = b.user_id
-GROUP BY user_id, first_name;
+GROUP BY 
+  user_id,  
+  first_name, 
+  role;
 
 -- Ranking properties based on the total number of bookings 
 -- Using window function RANK 
 -- Using Join also to show properties with zero bookings
 
 SELECT 
-p.property_id, 
-p.name AS property_name,
-COUNT(b.booking_id) AS total_bookings,
-RANK() OVER (
-ORDER BY COUNT(b.booking_id) DESC
-)  AS rank1,
-ROW_NUMBER() OVER (
-ORDER BY COUNT(b.booking_id) DESC
-)  AS row_rank
+  p.property_id, 
+  p.name AS property_name,
+  COUNT(b.booking_id) AS total_bookings,
+  RANK() OVER (
+  ORDER BY COUNT(b.booking_id) DESC
+  )  AS rank,
+  ROW_NUMBER() OVER (
+  ORDER BY COUNT(b.booking_id) DESC
+  )  AS row_rank
 FROM property p
 LEFT JOIN booking  b
 ON  p.property_id = b.property_id
-GROUP BY p.property_id, p.name;
+GROUP BY 
+  p.property_id, 
+  p.name;
+
 
 
 
