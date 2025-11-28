b9a01497e90b7a62cdb232b2fbd08224fce6a91c
danzeph <danzeph00@gmail.com>
2025-11-28 02:19:45 +0000

    Enhance performance with EXPLAIN and indexing
    Added EXPLAIN query and created indexes for performance.

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -25,6 +25,33 @@ LEFT JOIN payment pm ON b.booking_id = pm.booking_id
 WHERE pm.payment_id IS NOT NULL 
 AND b.status = 'confirmed';
 
+
+EXPLAIN SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.status,
+    b.total_price,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    CONCAT(u.first_name, ' ',u.last_name) AS user_name,
+    u.email,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    p.pricepernight,
+    pm.payment_id,
+    pm.amount AS payment_amount,
+    pm.payment_method
+FROM booking b
+JOIN users u ON b.user_id = u.user_id
+JOIN property p ON b.property_id = p.property_id
+LEFT JOIN payment pm ON b.booking_id = pm.booking_id
+WHERE pm.payment_id IS NOT NULL 
+AND b.status = 'confirmed';
+
+
 -- Creating Indexes on the booking status and start date
 -- Selecting only necessary colums 
 CREATE INDEX idx_booking_status ON booking(status);
