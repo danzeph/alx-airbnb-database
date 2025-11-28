c38f00911de9b34c79b6a2ccea6b1b35c49237bb
danzeph <danzeph00@gmail.com>
2025-11-28 01:55:24 +0000

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -9,6 +9,7 @@ SELECT
     u.user_id,
     u.first_name,
     u.last_name,
+    CONCAT(u.first_name, ' ',u.last_name) AS user_name,
     u.email,
     p.property_id,
     p.name AS property_name,
@@ -22,4 +23,26 @@ JOIN users u ON b.user_id = u.user_id
 JOIN property p ON b.property_id = p.property_id
 LEFT JOIN payment pm ON b.booking_id = pm.booking_id;
 
--- Creating Indexes on the 
+-- Creating Indexes on the booking status and start date
+-- Selecting only necessary colums 
+CREATE INDEX idx_booking_status ON booking(status);
+CREATE INDEX idx_booking_start_date ON booking(start_date);
+
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    p.property_id,
+    p.name AS property_name,
+    pm.payment_id,
+    pm.amount AS payment_amount
+FROM booking b
+JOIN users u ON b.user_id = u.user_id
+JOIN property p ON b.property_id = p.property_id
+LEFT JOIN payment pm ON b.booking_id = pm.booking_id;
+
+
