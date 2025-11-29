f43635126a0258d749b793e90c00cb5abce08cd4
danzeph <danzeph00@gmail.com>
2025-11-29 00:20:47 +0000

    Update partitioning.sql

--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -1,7 +1,7 @@
 --Partitioning the booking tablee based on start_date
 
 SELECT *,
-  RANK() OVER ( ORDER BY start_date DESC) AS ranks
+  RANK() OVER (PARTITION BY start_date ORDER BY end_date DESC) AS ranks
 FROM booking;
 
 
